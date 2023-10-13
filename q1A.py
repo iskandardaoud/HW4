fefb73b10e7593da3ce58691d9ac87c46c0ed150
iskandardaoud <144164712+iskandardaoud@users.noreply.github.com>
2023-10-12 21:24:02 -0700

    Update q1A.py

--- q1A.py
+++ q1A.py
@@ -1,1 +1,11 @@
-
+def fib(n):
+    if n <= 1:
+        return n
+    return fib (n - 1) + fib (n - 2)
+# Returns the number of ways to
+# reach the nth stair
+def countWays (s) :
+    return fib (s + 1)
+# Driver program
+s = int (input ("Enter no of steps: " ))
+print ("Number of ways =", countWays (s) )
