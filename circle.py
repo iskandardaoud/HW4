16c98e055e432779f775c3ee5c0df9eb151a9b94
iskandardaoud <144164712+iskandardaoud@users.noreply.github.com>
2023-10-12 22:49:40 -0700

    circle.py

--- circle.py
+++ circle.py
@@ -1,9 +1,26 @@
-# circle.py
-from shape import Shape
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
 
-class Circle(Shape):
+class Circle(Base):
     def __init__(self, x, y, size):
         super().__init__(x, y, size)
 
     def draw(self):
-        return f'Circle at ({self.x}, {self.y}) with radius {self.size}'
+        return f"""
+        ({self.x}, {self.y})
+        {self.size}
+        , - ~ ~ ~ - ,
+        , ' ' ,
+        , ,
+        , ,
+        , ,
+        , ,
+        , ,
+        , ,
+        , ,
+        , , '
+        ' - , _ _ _ , '
+        """
