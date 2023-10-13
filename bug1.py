d07045e4197fa8508da1b8cd0605ff4c33a7a85a
iskandardaoud <144164712+iskandardaoud@users.noreply.github.com>
2023-10-12 22:42:41 -0700

    bug1.py

--- bug1.py
+++ bug1.py
@@ -1,15 +1,8 @@
-from shape import Shape
+# bug1.py
 from circle import Circle
-
-class Base(Shape):
-    pass
-
-class Circle(Base):
-    pass
 
 def main():
     c = Circle(1, 2, 3)
-    print(c.shape())
     print(c.draw())
 
 if __name__ == '__main__':
