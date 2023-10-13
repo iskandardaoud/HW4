f18cc30458e3a3c8815f3f3bd97f1a27e872d58e
iskandardaoud <144164712+iskandardaoud@users.noreply.github.com>
2023-10-12 21:25:22 -0700

    Update q1A.py

--- q1A.py
+++ q1A.py
@@ -1,11 +1,27 @@
-def fib(n):
-    if n <= 1:
-        return n
-    return fib (n - 1) + fib (n - 2)
-# Returns the number of ways to
-# reach the nth stair
-def countWays (s) :
-    return fib (s + 1)
-# Driver program
-s = int (input ("Enter no of steps: " ))
-print ("Number of ways =", countWays (s) )
+
+def my_steps(n):
+    if not (1 <= n <= 25):
+        raise ValueError("Input is out of bounds. Please provide an integer
+between 1 and 25.")
+
+    # Create a list to store the number of ways for each step
+    ways = [0] * (n + 1)
+
+    # There is only one way to climb the first step
+    ways[1] = 1
+
+    # There are two ways to climb the second step
+    ways[2] = 2
+
+    # Calculate the number of ways to climb the ladder for each step
+    for i in range(3, n + 1):
+        ways[i] = ways[i - 1] + ways[i - 2]
+
+    return ways[n]
+
+# Example usage:
+# result = my_steps(2)
+# print(result)  # This should output 2
+
+# result = my_steps(3)
+# print(result)  # This should output 3
