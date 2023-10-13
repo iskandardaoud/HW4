797df54758f2b0a733a98f4295201553a9b5eca6
iskandardaoud <144164712+iskandardaoud@users.noreply.github.com>
2023-10-12 23:26:06 -0700

    bug1.py

--- bug1.py
+++ bug1.py
@@ -8,14 +8,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-In this revised code, we have organized the classes and imports correctly. The "Shape" class is defined in the "shape.py" module, and the "Circle" class inherits from it in the "circle.py" module. The "bug1.py" file imports the necessary classes and functions, and it should run without errors.
-
-Make sure your code files are organized in this way, and the circular import issue should be resolved.
-
-
-
-
-
 
 
 
